75bbaf59b7c48ce83241c6d33e3b43e7077a4dcf
luizvilelamarques <luizvilelamarques@gmail.com>
2020-05-14 12:31:21 -0300

    Update Teste.groovy

--- Teste.groovy
+++ Teste.groovy
@@ -3,13 +3,11 @@ class Teste {
   def _variavel = 0
 
   public void example1() {
-    println 'Hello from example1'
     _variavel = _variavel + 1
   }
 
-  public void example2() {
-    println 'Hello from example2'
-    println "valor variavel ${_variavel}"
+  public String example2() {
+   return _variavel
   }
 }
 
