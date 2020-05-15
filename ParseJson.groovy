7f6f9a4f087e9bb94dc7e3953af48d6603e4141f
luizvilelamarques <luizvilelamarques@gmail.com>
2020-05-15 15:23:33 -0300

    Update ParseJson.groovy

--- ParseJson.groovy
+++ ParseJson.groovy
@@ -5,9 +5,7 @@ def urlTxt
 @NonCPS
 def parse(){
    println '[DEBUG] Lendo arquivo json'
-   //return new JsonSlurperClassic().parse(urlTxt.toURL())
-   return new JsonSlurper().parse(urlTxt.toURL())
-   
+   return new JsonSlurperClassic().parse(urlTxt.toURL())
 }
 
 def instancia(url){
