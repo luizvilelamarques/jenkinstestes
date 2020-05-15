39a7a3665eeb2c8570b20e161a7fda0b6caf43d7
luizvilelamarques <luizvilelamarques@gmail.com>
2020-05-15 13:22:31 -0300

    Update Test.groovy

--- Test.groovy
+++ Test.groovy
@@ -2,16 +2,16 @@ def test
 
 def instancia(buildInfo, groovyBase){
      if (buildInfo.tipoProjeto == 'maven'){
-         test = dynamicLoad(groovyBase, '/TestMaven.groovy').instancia(buildInfo)
+         test = dynamicLoad(groovyBase + '/', 'TestMaven.groovy').instancia(buildInfo)
      }else if (buildInfo.tipoProjeto == 'node'){ 
-         test = dynamicLoad(groovyBase, '/TestNode.groovy').instancia(buildInfo)
+         test = dynamicLoad(groovyBase + '/', 'TestNode.groovy').instancia(buildInfo)
      }
      return this
 }
 
 def dynamicLoad(groovyBase, scriptName){
      sh "wget ${_groovyBase}${scriptName}"
-     return load "${scriptName}"
+     return load "'${scriptName}'"
 }
 
 return this
