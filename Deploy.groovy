381aef7982009bcdb8a0fa64bf15fc042daace03
luizvilelamarques <luizvilelamarques@gmail.com>
2020-05-15 16:04:26 -0300

    Create Deploy.groovy

---
+++ Deploy.groovy
@@ -0,0 +1,21 @@
+def deployRef
+
+def instancia(deployInfoParam, groovyBase){
+    deployInfo = deployInfoParam
+    if (deployInfoParam.okd!=null) {
+      deployRef = dynamicLoad(groovyBase + "/", "DeployOKD.groovy").instancia(deployInfoParam)
+    }
+    return this
+}
+
+def deploy(){
+    deployRef.deploy()     
+}
+
+def dynamicLoad(groovyBase, scriptName){
+     sh "wget ${groovyBase}${scriptName}"
+     code = load "${scriptName}"
+     return code
+}
+
+return this
