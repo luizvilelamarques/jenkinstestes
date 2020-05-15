62ecf4ff23ac57cc59dc2f3833d337a8de76c8b5
luizvilelamarques <luizvilelamarques@gmail.com>
2020-05-15 12:41:21 -0300

    Update Build.groovy

--- Build.groovy
+++ Build.groovy
@@ -2,14 +2,14 @@ def build
 
 def instancia(buildInfo, groovyBase){
      if (buildInfo.tipoProjeto == 'maven'){
-         build = factory('BuildMaven.groovy').instancia(buildInfo)
+         build = factory(groovyBase, 'BuildMaven.groovy').instancia(buildInfo)
      }else if (buildInfo.tipoProjeto == 'node'){ 
-          build = factory('BuildNode.groovy').instancia(buildInfo)
+          build = factory(groovyBase, 'BuildNode.groovy').instancia(buildInfo)
      }
      return this
 }
 
-def factory(scriptName){
+def factory(groovyBase, scriptName){
      sh "wget ${_groovyBase}/${scriptName}"
      return load "${scriptName}"
 }
