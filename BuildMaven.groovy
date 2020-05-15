6555106efbb5ce80327f5f0a32e2a29c296dfb67
luizvilelamarques <luizvilelamarques@gmail.com>
2020-05-15 16:18:04 -0300

    Update BuildMaven.groovy

--- BuildMaven.groovy
+++ BuildMaven.groovy
@@ -6,7 +6,10 @@ def instancia(buildInfo){
 }
 
 def build(){
-    sh " mvn clean install -f ${pacote}"
+     def mvn_version = 'Maven 3.6.3'
+     withEnv( ["PATH+MAVEN=${tool mvn_version}/bin"] ) {
+          sh " mvn clean install -f ${pacote}"
+     }
 }
 
 return this
