bb894671535394ac6eed4e8c3aa605e2044af36a
luizvilelamarques <luizvilelamarques@gmail.com>
2020-05-15 14:55:32 -0300

    Update Build.groovy

--- Build.groovy
+++ Build.groovy
@@ -1,6 +1,7 @@
 def buildRef
 
 def instancia(buildInfo, groovyBase){
+     sh "echo tipo de projeto:  ${buildInfo.tipoProjeto}" 
      if ("maven" == buildInfo.tipoProjeto){
          buildRef = dynamicLoad(groovyBase + "/", "BuildMaven.groovy").instancia(buildInfo)
      }else if ("node" == buildInfo.tipoProjeto){ 
